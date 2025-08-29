ad70d74166027898a59c2949cc75297e60190530
enrlzzz <enricolg16@gmail.com>
2025-08-28 23:04:12 -0300

    app em java que calcula a area de um circulo com base em seu diametro

---
+++ areadacircunferencia/AreaDaCircunferencia.java
@@ -0,0 +1,19 @@
+package areadacircunferencia;
+
+
+public class AreaDaCircunferencia {
+
+    
+    public static void main(String[] args) {
+       final double PI = 3.14159;
+       
+       double raio = 5.0;
+       
+       double area = PI * (raio * raio);
+       
+        System.out.println("A area da circunferencia com raio de " + raio + " cm e de " + area + " cm quadrados ");
+        
+        
+    }
+    
+}
